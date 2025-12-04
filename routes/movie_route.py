bc70d8b1d4c2f3083c34234a95cf9c81a0d9b25b
Plep-m <menut34@gmail.com>
2025-12-04 19:17:25 +0100

    wip movie view

--- routes/movie_route.py
+++ routes/movie_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from services.movie_service import movieService
 
 bp = Blueprint('movie', __name__)
@@ -6,5 +6,23 @@ bp = Blueprint('movie', __name__)
 @bp.route('/movies')
 def movies():
     service = movieService()
-    movies = service.get_movies_paginated(page=1, page_size=20)
-    return render_template("movies.html", movies=movies)
+    page_size = 20
+    page_arg = request.args.get('page', '1')
+    try:
+        page = int(page_arg)
+        if page < 1:
+            page = 1
+    except ValueError:
+        page = 1
+
+    movies_fetched = service.get_movies_paginated(page=page, page_size=page_size + 1)
+
+    has_next = len(movies_fetched) > page_size
+    movies = movies_fetched[:page_size]  # show only page_size items
+
+    return render_template(
+        "movies.html",
+        movies=movies,
+        page=page,
+        has_next=has_next
+    )
